babd0f1393e2ab7b2bb1a8ca084752fdf286dbf9
Evgeny Blokhin <eb@tilde.pro>
2021-09-08 00:40:12 +0200

    Add Postgres schema

---
+++ i_data/data_items.sql
@@ -0,0 +1,9 @@
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE data_items (
+    item_id UUID NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
+    label VARCHAR(256),
+    content VARCHAR,
+    type SMALLINT DEFAULT 0
+);
