a30359c27dba7dc31341a6b29e64ad080e30b48c
Evgeny Blokhin <eb@tilde.pro>
2021-09-22 16:39:35 +0200

    Update configs and deployment (still unfinished)

--- conf/install.sh
+++ conf/install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+PG_SOURCE_ADDR=https://ftp.postgresql.org/pub/source/v13.4/postgresql-13.4.tar.gz # NB subject to update
+PG_VERSION="13.4" # NB subject to update
+
+SETTINGS=(
+postgresql.conf
+supervisord.conf
+)
+
+for ((i=0; i<${#SETTINGS[@]}; i++)); do
+    if [ ! -f $(dirname $0)/${SETTINGS[i]} ]; then
+    echo "${SETTINGS[i]} does not exist" ; exit 1
+    fi
+done
+
+
 apt-get -y update && apt-get -y upgrade
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 apt-get -y install build-essential nginx libatlas-base-dev libopenblas-dev libblas-dev libffi-dev libreadline6-dev zlib1g-dev liblapack-dev supervisor python3-dev python3-pip python3-numpy python3-scipy python3-matplotlib p7zip-full git swig python3-setuptools rabbitmq-server pkg-config
@@ -8,4 +23,36 @@ update-rc.d supervisor defaults
 update-rc.d supervisor enable
 
 echo "set mouse-=a" > ~/.vimrc
-mkdir /data
+
+# Postgres compilation and installation
+
+useradd postgres
+mkdir -p /data/pg
+mkdir /data/pg/db
+chown -R postgres:postgres /data/pg
+
+wget $PG_SOURCE_ADDR
+wget $PG_SOURCE_ADDR.md5
+GOT_SUM=`md5sum *.tar.gz | cut -d" " -f1`
+HAVE_SUM=`cut -d' ' -f1 < *.tar.gz.md5`
+if [ "$GOT_SUM" != "$HAVE_SUM" ]; then
+    echo "INVALID CHECKSUM"
+    exit 1
+fi
+
+tar xvf postgresql-$PG_VERSION.tar.gz
+cd postgresql-$PG_VERSION
+./configure --prefix=/data/pg
+make && make install
+
+cd contrib/pgcrypto
+PG_CONFIG=/data/pg/bin/pg_config make
+PG_CONFIG=/data/pg/bin/pg_config make install
+
+su postgres -c "/data/pg/bin/initdb -D /data/pg/db"
+su postgres -c "/data/pg/bin/pg_ctl -D /data/pg/db -l /tmp/logfile start"
+su postgres -c "/data/pg/bin/createdb aiidadb"
+
+chown -R postgres:postgres /data/pg
+cd ..
+cp $(dirname $0)/postgresql.conf /data/pg/db/
