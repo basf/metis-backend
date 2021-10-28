0feffc14c001330aae9f1fbe14f54fe9e02aefbd
Evgeny Blokhin <eb@tilde.pro>
2021-10-29 01:01:59 +0200

    Add standalone crystal structure utils

---
+++ i_structures/cif_utils.py
@@ -0,0 +1,173 @@
+
+import tempfile # used by pycodcif; FIXME
+import re
+
+import numpy as np
+
+from pycodcif import parse
+
+from ase import Atom
+from ase.spacegroup import crystal
+from ase.geometry import cell_to_cellpar
+
+
+def cif_to_ase(cif_string):
+    """
+    Naive pycodcif usage
+    FIXME:
+    as soon as pycodcif supports CIFs as strings,
+    the tempfile below should be removed
+
+    Args:
+        cif_string: (str)
+
+    Returns:
+        ASE atoms (object) *or* None
+        None *or* error (str)
+    """
+
+    # empty data_ keyword fixup
+    for check in ["\ndata_\n", "\ndata_\r", "\rdata_\r"]:
+        if check in cif_string:
+            cif_string = cif_string.replace(check, check.replace('_', '_new'))
+            break
+
+    with tempfile.NamedTemporaryFile(suffix='.cif') as tmp:
+        tmp.write(cif_string.encode('utf-8'))
+        tmp.flush()
+
+        try:
+            parsed_cif = parse(tmp.name)[0][0]['values']
+        except:
+            return None, 'Invalid or non-standard CIF'
+
+        if '_symmetry_int_tables_number' in parsed_cif:
+            try:
+                spacegroup = int(parsed_cif['_symmetry_int_tables_number'][0])
+            except ValueError:
+                return None, 'Invalid space group info in CIF'
+
+        elif '_symmetry_space_group_name_h-m' in parsed_cif:
+            spacegroup = parsed_cif['_symmetry_space_group_name_h-m'][0].strip() # NB ase is very strict to whitespaces in HM symbols, so this is the most frequent error source
+            if not spacegroup:
+                return None, 'Empty space group info in CIF'
+
+        else: return None, 'Absent space group info in CIF'
+
+        try:
+            cellpar = (
+                float( parsed_cif['_cell_length_a'][0].split('(')[0] ),
+                float( parsed_cif['_cell_length_b'][0].split('(')[0] ),
+                float( parsed_cif['_cell_length_c'][0].split('(')[0] ),
+                float( parsed_cif['_cell_angle_alpha'][0].split('(')[0] ),
+                float( parsed_cif['_cell_angle_beta'][0].split('(')[0] ),
+                float( parsed_cif['_cell_angle_gamma'][0].split('(')[0] )
+            )
+            basis = np.transpose(
+                np.array([
+                    [ char.split('(')[0] for char in parsed_cif['_atom_site_fract_x'] ],
+                    [ char.split('(')[0] for char in parsed_cif['_atom_site_fract_y'] ],
+                    [ char.split('(')[0] for char in parsed_cif['_atom_site_fract_z'] ]
+                ]).astype(np.float)
+            )
+            occupancies = [float(occ.split('(')[0]) for occ in parsed_cif.get('_atom_site_occupancy', [])]
+        except:
+            return None, 'Unexpected non-numerical values occured in CIF'
+
+    symbols = parsed_cif.get('_atom_site_type_symbol')
+
+    if not symbols:
+        symbols = parsed_cif.get('_atom_site_label')
+        if not symbols:
+            return None, 'Cannot find atomic positions in CIF'
+
+    non_els = re.compile(r'[^a-zA-Z]')
+    symbols = [non_els.sub('', char) for char in symbols]
+
+    occ_data = None
+    if occupancies and any([occ != 1 for occ in occupancies]):
+        basis = basis.tolist()
+        partial_pos, occ_data = {}, {}
+        for n in range(len(occupancies) - 1, -1, -1):
+            if occupancies[n] != 1:
+                disordered_pos = basis.pop(n)
+                disordered_el = symbols.pop(n)
+                partial_pos.setdefault(tuple(disordered_pos), {})[disordered_el] = occupancies[n]
+
+        for xyz, occs in partial_pos.items():
+            index = len(symbols)
+            symbols.append(sorted(occs.keys())[0])
+            basis.append(xyz)
+            occ_data[index] = occs
+
+    atom_data = []
+    for n, xyz in enumerate(basis):
+        try:
+            atom_data.append(Atom(symbols[n], tuple(xyz), tag=n))
+        except KeyError as exc:
+            return None, 'Unrecognized atom symbol: %s' % exc
+
+    try:
+        return crystal(
+            atom_data,
+            spacegroup=spacegroup,
+            cellpar=cellpar,
+            primitive_cell=True,
+            onduplicates='error',
+            info=dict(disordered=occ_data) if occ_data else {}
+        ), None
+    except:
+        return None, 'Unrecognized sites or invalid site symmetry in CIF'
+
+
+def ase_to_eq_cif(ase_obj, supply_sg=True, mpds_labs_loop=None):
+    """
+    From ASE object generate CIF
+    with symmetry-equivalent atoms;
+    augment with the aux info, if needed
+    """
+    cif_data  = 'data_mpds_labs\n'
+
+    if type(mpds_labs_loop) == list:
+        cif_data += '\n_mpds_prediction_quality %s/%s' % (mpds_labs_loop[0], len(mpds_labs_loop) - 1) + "\n"
+        cif_data += '\nloop_' + "\n"
+        cif_data += ' _mpds_labs_property' + "\n"
+        cif_data += ' _user_requested_min' + "\n"
+        cif_data += ' _mpds_labs_value' + "\n"
+        cif_data += ' _user_requested_max' + "\n"
+        cif_data += ' _mpds_labs_units' + "\n"
+        for deck in mpds_labs_loop[1:]:
+            cif_data += " '" + "' '".join(map(str, deck)) + "'\n"
+
+    parameters = cell_to_cellpar(ase_obj.cell)
+    cif_data += '_cell_length_a    ' + "%2.6f" % parameters[0] + "\n"
+    cif_data += '_cell_length_b    ' + "%2.6f" % parameters[1] + "\n"
+    cif_data += '_cell_length_c    ' + "%2.6f" % parameters[2] + "\n"
+    cif_data += '_cell_angle_alpha ' + "%2.6f" % parameters[3] + "\n"
+    cif_data += '_cell_angle_beta  ' + "%2.6f" % parameters[4] + "\n"
+    cif_data += '_cell_angle_gamma ' + "%2.6f" % parameters[5] + "\n"
+
+    if supply_sg:
+        sg_string = "_symmetry_space_group_name_H-M '%s'\n_symmetry_Int_Tables_number %s\n" % (
+            getattr(ase_obj.info.get('spacegroup', object), 'symbol', 'P1'),
+            getattr(ase_obj.info.get('spacegroup', object), 'no', 1)
+        )
+    else:
+        sg_string = "_symmetry_space_group_name_H-M 'P1'\n_symmetry_Int_Tables_number 1\n"
+
+    cif_data += sg_string
+    cif_data += '\nloop_' + "\n"
+    cif_data += ' _symmetry_equiv_pos_as_xyz' + "\n"
+    cif_data += ' +x,+y,+z' + "\n"
+
+    cif_data += '\nloop_' + "\n"
+    cif_data += ' _atom_site_type_symbol' + "\n"
+    cif_data += ' _atom_site_fract_x' + "\n"
+    cif_data += ' _atom_site_fract_y' + "\n"
+    cif_data += ' _atom_site_fract_z' + "\n"
+
+    pos = ase_obj.get_scaled_positions(wrap=False)
+    for n, item in enumerate(ase_obj):
+        cif_data += " {:3s} {: 6.3f} {: 6.3f} {: 6.3f}\n".format(item.symbol, pos[n][0], pos[n][1], pos[n][2])
+
+    return cif_data
---
+++ i_structures/struct_utils.py
@@ -0,0 +1,305 @@
+
+import math
+import random
+import itertools
+import fractions
+from functools import reduce
+from io import StringIO
+
+import ujson as json
+from ase.atoms import Atom, Atoms
+from ase.io.vasp import read_vasp
+from ase.spacegroup import crystal
+
+import spglib
+
+
+def detect_format(string):
+    """
+    Detect CIF or POSCAR
+    checking the most common features
+    """
+    if '_cell_angle_gamma' in string \
+    and 'loop_' in string:
+        return 'cif'
+
+    lines = string.splitlines()
+
+    for nline in [6, 7, 8]:
+        if len(lines) <= nline:
+            break
+        if lines[nline].strip().lower().startswith('direct') \
+        or lines[nline].strip().lower().startswith('cart'):
+            return 'poscar'
+
+    if '"immutable_id"' in string and '"cartesian_site_positions"' in string and '"lattice_vectors"' in string:
+        return 'optimade'
+
+    return None
+
+
+def poscar_to_ase(poscar_string):
+    """
+    Parse POSCAR using ase
+
+    Returns:
+        Refined ASE structure (object) *or* None
+        None *or* error (str)
+    """
+    ase_obj, error = None, None
+    buff = StringIO(poscar_string)
+    try:
+        ase_obj = read_vasp(buff)
+    except AttributeError:
+        error = 'Types of atoms can be neither found nor inferred'
+    except Exception:
+        error = 'Cannot process POSCAR: invalid or missing data'
+    buff.close()
+    return ase_obj, error
+
+
+def json_to_ase(datarow):
+    """
+    An extended *mpds_client* static *compile_crystal* method
+    for handling the disordered structures
+    in an oversimplified, very narrow-purpose way
+
+    TODO?
+    Avoid els_noneq rewriting
+    """
+    if not datarow or not datarow[-1]:
+        return None, "No structure found"
+
+    occs_noneq, cell_abc, sg_n, basis_noneq, els_noneq = \
+        datarow[-5], datarow[-4], int(datarow[-3]), datarow[-2], datarow[-1]
+
+    occ_data = None
+    if any([occ != 1 for occ in occs_noneq]):
+        partial_pos, occ_data = {}, {}
+        for n in range(len(occs_noneq) - 1, -1, -1):
+            if occs_noneq[n] != 1:
+                disordered_pos = basis_noneq.pop(n)
+                disordered_el = els_noneq.pop(n)
+                partial_pos.setdefault(tuple(disordered_pos), {})[disordered_el] = occs_noneq[n]
+
+        for xyz, occs in partial_pos.items():
+            index = len(els_noneq)
+            els_noneq.append(sorted(occs.keys())[0])
+            basis_noneq.append(xyz)
+            occ_data[index] = occs
+
+    atom_data = []
+
+    for n, xyz in enumerate(basis_noneq):
+        atom_data.append(Atom(els_noneq[n], tuple(xyz), tag=n))
+
+    if not atom_data:
+        return None, "No atoms found"
+
+    try:
+        return crystal(
+            atom_data,
+            spacegroup=sg_n,
+            cellpar=cell_abc,
+            primitive_cell=True,
+            onduplicates='error',
+            info=dict(disordered=occ_data) if occ_data else {}
+        ), None
+    except Exception as ex:
+        return None, "ASE cannot handle structure: %s" % ex
+
+
+def optimade_to_ase(structure):
+    """
+    A very permissive Optimade format support
+    so far WITHOUT the disordered structures
+
+    Returns:
+        ASE Atoms (object) *or* None
+        None *or* error (str)
+    """
+    if type(structure) == str:
+        structure = json.loads(structure)
+
+    if 'data' in structure and type(structure['data']) == list and len(structure['data']):
+        structure = structure['data'][0]
+
+    basis, symbols = [], []
+
+    if 'species' not in structure['attributes']:
+        return None, "Atoms missing"
+
+    for n, specie in enumerate(structure['attributes']['species']):
+
+        if len(specie['chemical_symbols']) > 1:
+            if 'concentration' not in specie:
+                return None, "Atomic disorder data incomplete"
+
+            return None, "Structural disorder is not supported"
+
+        symbols.append(specie['chemical_symbols'][0])
+        basis.append(structure['attributes']['cartesian_site_positions'][n])
+
+    return Atoms(
+        symbols=symbols,
+        positions=basis,
+        cell=structure['attributes']['lattice_vectors'],
+        pbc=structure['attributes'].get('dimension_types') or True
+    ), None
+
+
+def refine(ase_obj, accuracy=1E-03, conventional_cell=False):
+    """
+    Refine ASE structure using spglib
+
+    Args:
+        ase_obj: (object) ASE structure
+        accuracy: (float) spglib tolerance, normally within [1E-02, 1E-04]
+
+    Returns:
+        Refined ASE structure (object) *or* None
+        None *or* error (str)
+    """
+    try:
+        symmetry = spglib.get_spacegroup(ase_obj, symprec=accuracy)
+        lattice, positions, numbers = spglib.standardize_cell(ase_obj, symprec=accuracy, to_primitive=not conventional_cell)
+    except:
+        return None, 'Error while structure refinement'
+
+    try:
+        spacegroup = int( symmetry.split()[1].replace("(", "").replace(")", "") )
+    except (ValueError, IndexError, AttributeError):
+        return None, 'Symmetry error (coinciding atoms?) in structure'
+
+    try:
+        return crystal(
+            Atoms(
+                numbers=numbers,
+                cell=lattice,
+                scaled_positions=positions,
+                pbc=True
+            ),
+            spacegroup=spacegroup,
+            primitive_cell=not conventional_cell,
+            onduplicates='replace'
+        ), None
+    except:
+        return None, 'Unrecognized sites or invalid site symmetry in structure'
+
+
+FORMULA_SEQUENCE = ['Fr','Cs','Rb','K','Na','Li',  'Be','Mg','Ca','Sr','Ba','Ra',  'Sc','Y','La','Ce','Pr','Nd','Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb',  'Ac','Th','Pa','U','Np','Pu',  'Ti','Zr','Hf',  'V','Nb','Ta',  'Cr','Mo','W',  'Fe','Ru','Os',  'Co','Rh','Ir',  'Mn','Tc','Re',  'Ni','Pd','Pt',  'Cu','Ag','Au',  'Zn','Cd','Hg',  'B','Al','Ga','In','Tl',  'Pb','Sn','Ge','Si','C',   'N','P','As','Sb','Bi',   'H',   'Po','Te','Se','S','O',  'At','I','Br','Cl','F',  'He','Ne','Ar','Kr','Xe','Rn']
+
+def get_formula(ase_obj, find_gcd=True, as_dict=False):
+    parsed_formula = {}
+
+    for label in ase_obj.get_chemical_symbols():
+        if label not in parsed_formula:
+            parsed_formula[label] = 1
+        else:
+            parsed_formula[label] += 1
+
+    expanded = reduce(fractions.gcd, parsed_formula.values()) if find_gcd else 1
+    if expanded > 1:
+        parsed_formula = {el: int(content / float(expanded))
+                        for el, content in parsed_formula.items()}
+
+    if as_dict: return parsed_formula
+
+    atoms = parsed_formula.keys()
+    atoms = [x for x in FORMULA_SEQUENCE if x in atoms] + [x for x in atoms if x not in FORMULA_SEQUENCE]
+    formula = ''
+    for atom in atoms:
+        index = parsed_formula[atom]
+        index = '' if index == 1 else str(index)
+        formula += atom + index
+
+    return formula
+
+
+def sgn_to_crsystem(number):
+    if   195 <= number <= 230:
+        return 'cubic'
+    elif 168 <= number <= 194:
+        return 'hexagonal'
+    elif 143 <= number <= 167:
+        return 'trigonal'
+    elif 75  <= number <= 142:
+        return 'tetragonal'
+    elif 16  <= number <= 74:
+        return 'orthorhombic'
+    elif 3   <= number <= 15:
+        return 'monoclinic'
+    else:
+        return 'triclinic'
+
+
+MAX_ATOMS = 1000
+SITE_SUM_OCCS_TOL = 0.99
+
+def order_disordered(ase_obj):
+    """
+    This is a toy algo to get rid of the structural disorder;
+    just one random possible ordered structure is returned
+    (out of may be billions). No attempt to embrace all permutations is made.
+    For that one needs to consider the special-purpose software (e.g.
+    https://doi.org/10.1186/s13321-016-0129-3 etc.).
+
+    Args:
+        ase_obj: (object) ASE structure; must have *info* dict *disordered* and *Atom* tags
+            *disordered* dict format: {'disordered': {at_index: {element: occupancy, ...}, ...}
+
+    Returns:
+        ASE structure (object) *or* None
+        None *or* error (str)
+
+    TODO?
+    Rewrite space group info accordingly
+    """
+    for index in ase_obj.info['disordered']:
+        if sum(ase_obj.info['disordered'][index].values()) < SITE_SUM_OCCS_TOL:
+            ase_obj.info['disordered'][index].update(
+                {'X': 1 - sum(ase_obj.info['disordered'][index].values())}
+            )
+
+    min_occ = min(
+        sum(
+            [list(item.values()) for item in ase_obj.info['disordered'].values()],
+            []
+        )
+    )
+    if min_occ == 0:
+        return None, 'Zero occupancy is encountered'
+
+    needed_det = math.ceil(1. / min_occ)
+    if needed_det * len(ase_obj) > MAX_ATOMS:
+        return None, 'Resulting crystal cell size is too big'
+
+    diag = needed_det ** (1. / 3)
+    supercell_matrix = [int(x) for x in (round(diag), math.ceil(diag), math.ceil(diag))]
+    actual_det = reduce(lambda x, y: x * y, supercell_matrix)
+
+    occ_data = {}
+    for index, occs in ase_obj.info['disordered'].items():
+        disorder = []
+        for el, occ in occs.items():
+            disorder += [el] * int(round(occ * actual_det))
+        random.shuffle(disorder)
+        occ_data[index] = itertools.cycle(disorder)
+
+    order_obj = ase_obj.copy()
+    order_obj *= supercell_matrix
+    del order_obj.info['disordered']
+
+    for index, occs in occ_data.items():
+        for n in range(len(order_obj) - 1, -1, -1):
+            if order_obj[n].tag == index:
+                distrib_el = next(occs)
+                if distrib_el == 'X':
+                    del order_obj[n]
+                else:
+                    try:
+                        order_obj[n].symbol = distrib_el
+                    except KeyError as exc:
+                        return None, 'Unrecognized atom symbol: %s' % exc
+
+    return order_obj, None
