eb4ddd78ff4ed16cb83f1c8720fd8fb816c68160
Evgeny Blokhin <eb@tilde.pro>
2021-10-29 01:05:04 +0200

    Update login test

--- tests/integration/test_login.py
+++ tests/integration/test_login.py
@@ -6,12 +6,12 @@ from utils import make_request
 host = 'http://localhost:3000'
 #host = 'https://gate.basf.science/v0'
 
-headers, answer = make_request(host + '/users/login', {'login': 'basf', 'password': 'fsab'}, 'POST')
+headers, answer = make_request(host + '/auth', {'email': 'test@test.com', 'password': '123123'}, 'POST')
 
 assert headers.get('set-cookie')
 
 user_session = headers.get('set-cookie')
 
-_, answer = make_request(host + '/users/me', {'empty': 'empty'}, 'GET', headers={'Cookie': user_session})
+_, answer = make_request(host + '/auth', {}, 'GET', headers={'Cookie': user_session})
 
 print(answer)
