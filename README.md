bd5571736a3032d62c03229e10380b11dcfad1da
Evgeny Blokhin <eb@tilde.pro>
2021-12-10 01:49:29 +0100

    Add install details

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 ### Requirements
 
-See `conf/install.sh`.
+The core requirements are Python and Postgres. See `conf/install.sh`.
 
 ## Installation
 
 Run `conf/install.sh` and then modify `conf/env.ini`.
+
+Then setup your virtual env and install the Python requirements `pip install -r requirements.txt`.
 
 ## Running
 
--- conf/install.sh
+++ conf/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PG_VERSION="13.5" # NB subject to update
+PG_VERSION="14.1" # NB subject to update
 PG_SOURCE_ADDR=https://ftp.postgresql.org/pub/source/v$PG_VERSION/postgresql-$PG_VERSION.tar.gz
 
 SETTINGS=(
@@ -17,7 +17,7 @@ done
 
 apt-get -y update && apt-get -y upgrade
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
-apt-get -y install build-essential nginx libatlas-base-dev libopenblas-dev libblas-dev libffi-dev libreadline6-dev zlib1g-dev liblapack-dev supervisor python3-dev python3-pip python3-numpy python3-scipy python3-matplotlib p7zip-full git swig python3-setuptools rabbitmq-server pkg-config
+apt-get -y install build-essential nginx libatlas-base-dev libopenblas-dev libblas-dev libffi-dev libreadline6-dev zlib1g-dev liblapack-dev supervisor python3-dev python3-pip python3-numpy python3-scipy python3-matplotlib p7zip-full git swig python3-setuptools pkg-config
 
 update-rc.d supervisor defaults
 update-rc.d supervisor enable
