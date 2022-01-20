a2e9798a7976f8ade7e6ba7887204a6be0aeb35e
Evgeny Blokhin <eb@tilde.pro>
2022-01-20 01:13:34 +0100

    Polish deps and schema

--- i_data/schema.sql
+++ i_data/schema.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE bscience_data_items (
     item_id UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    label VARCHAR(256),
+    name VARCHAR(256),
     content VARCHAR,
     type SMALLINT
 );
