9086d9338f3d95281ec5c44298a84869a96ac876
Evgeny Blokhin <eb@tilde.pro>
2022-09-14 21:01:11 +0200

    Add cleaning SQL

---
+++ conf/clean_test_data.sql
@@ -0,0 +1,5 @@
+TRUNCATE yascheduler_tasks CASCADE;
+TRUNCATE bff_user_datasources CASCADE;
+TRUNCATE bff_user_calculations CASCADE;
+TRUNCATE backend_data_nodes CASCADE;
+TRUNCATE backend_data_links CASCADE;
