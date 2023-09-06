5534e05d1610a217fbb368d82bc394816576f3fc
Evgeny Blokhin <eb@tilde.pro>
2023-09-06 22:22:31 +0200

    Add phase assoc algo (beta version TODO)

---
+++ scripts/assign_phases.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+"""
+This script deals with both the backend and BFF tables,
+analyzing and updating them. To be run by cron every 5 minutes.
+"""
+import logging
+
+import spglib
+
+from phase_utils import assign_phase, save_as_phase
+
+import set_path
+from i_data import Data_type, NODE_TABLE, PHASE_TABLE
+from i_structures import html_formula
+from i_structures.struct_utils import get_formula, sgn_to_crsystem, crsystem_to_sgn, ase_unserialize
+from utils import get_data_storage
+
+
+accuracy = 1E-02
+
+main_conn = get_data_storage()
+work_conn = get_data_storage()
+
+seen = []
+
+logging.basicConfig(level=logging.WARNING)
+
+main_conn.cursor.execute("SELECT item_id, metadata, content FROM {} WHERE type = {} AND has_phase = FALSE;".format(
+    NODE_TABLE, Data_type.structure
+))
+for row in main_conn.cursor.fetchall():
+
+    ase_obj = ase_unserialize(row[2])
+    symmetry = spglib.get_spacegroup(ase_obj, symprec=accuracy)
+    try:
+        sgn = int( symmetry.split()[1].replace("(", "").replace(")", "") )
+    except (ValueError, IndexError, AttributeError):
+        logging.error('Symmetry error (coinciding atoms?) in structure')
+        continue
+
+    elements = sorted(list( set([atom.symbol for atom in ase_obj]) ))
+
+    logging.info(('WORKING ON', get_formula(ase_obj), sgn))
+
+    work_conn.cursor.execute("SELECT phase_id, formula, spg FROM {} WHERE elements = '-{}-' AND spg = {};".format(
+        PHASE_TABLE, "--".join(elements), sgn
+    ))
+    found = work_conn.cursor.fetchone()
+    result = None
+
+    # EXACT MATCHING
+    if found:
+        phid, label, spg = found
+        #label = html_formula(label)
+        logging.info(('FOUND', row[1]['name'], phid, label, spg))
+        result = (phid, label, spg)
+
+    # FUZZY MATCHING
+    else:
+        logging.info(('TRYING', sgn_to_crsystem(sgn)))
+        sg1, sg2 = crsystem_to_sgn(sgn_to_crsystem(sgn))
+        work_conn.cursor.execute("SELECT phase_id, formula, spg FROM {} WHERE elements = '-{}-' AND spg >= {} AND spg <= {};".format(
+            PHASE_TABLE, "--".join(elements), sg1, sg2
+        ))
+        candidates = []
+        for candidate in work_conn.cursor.fetchall():
+            logging.info(('MAYBE', row[1]['name'], candidate, sgn))
+            candidates.append(candidate)
+
+        if candidates:
+            result = assign_phase((get_formula(ase_obj), sgn), candidates)
+            #if assigned:
+            #    result = (assigned[0], html_formula(assigned[1]), assigned[2])
+        else:
+            logging.error('Cannot determine phase for %s (%s)' % (row[1]['name'], sgn))
+
+    if result:
+        logging.info((row[1]['name'], 'will be assigned phase', result))
+        save_as_phase(work_conn, row[0], result)
+
+    seen.append(str(row[0]))
+
+work_conn.close()
+
+if seen:
+    main_conn.cursor.execute("UPDATE {} SET has_phase = TRUE WHERE item_id IN ('{}');".format(
+        NODE_TABLE, "', '".join(seen)
+    ))
+    main_conn.connection.commit()
+
+main_conn.close()
---
+++ scripts/phase_utils.py
@@ -0,0 +1,94 @@
+
+import re
+import logging
+import random # TODO
+
+import set_path
+from i_structures.chemical_formulae import parse_formula
+
+
+BFF_PREFIX = 'bff_'
+
+USERID = 3 # FIXME Metis Robot
+TYPEID = 4 # FIXME slug=phases
+
+PHASE_DESCR = 'https://mpds.io/phase_id/'
+
+BFF_TABLES = {
+    'datasources':             BFF_PREFIX + 'user_datasources',
+    'collections':             BFF_PREFIX + 'user_collections',
+    'collections_datasources': BFF_PREFIX + 'user_collections_datasources',
+}
+
+COLLECTION_VISIBILITY = {
+    'private': 'private',
+    'shared': 'shared',
+    'community': 'community',
+}
+
+
+def assign_phase(given, candidates):
+    # TODO
+    return random.choice(candidates)
+
+
+def clean_formula_tags(string):
+    return string.replace('<sub>', '').replace('</sub>', '')
+
+
+fidx = re.compile(r"(\d)")
+def formula_to_chars(string):
+    return fidx.sub("&#x208\g<0>;", string)
+
+
+def create_tag(conn, cursor, user_id, type_id, title, descr, visibility=COLLECTION_VISIBILITY['community']):
+    cursor.execute("""
+    INSERT INTO {tags_table} ("userId", "typeId", title, description, visibility) VALUES
+    ({user_id}, {type_id}, '{title}', '{descr}', '{visibility}')
+    RETURNING id;
+    """.format(
+        tags_table=BFF_TABLES['collections'],
+        user_id=user_id,
+        type_id=type_id,
+        title=title,
+        descr=descr,
+        visibility=visibility
+    ))
+    conn.commit()
+    return cursor.fetchone()[0]
+
+
+def get_or_create_tag(conn, cursor, user_id, type_id, title, descr):
+    cursor.execute("""SELECT id FROM {} WHERE title = '{}';""".format(BFF_TABLES['collections'], title))
+    found = cursor.fetchone()
+    return found[0] if found else create_tag(conn, cursor, user_id, type_id, title, descr)
+
+
+def save_as_phase(db, node_uuid, phase):
+
+    logging.warning(f"Assing phase {phase} to node {node_uuid}")
+
+    db.cursor.execute("""SELECT id FROM {} WHERE uuid = '{}';""".format(
+        BFF_TABLES['datasources'], node_uuid
+    ))
+    found = db.cursor.fetchone()
+    if not found:
+        logging.critical(f"Node {node_uuid} is orphaned")
+        return
+
+    datasrc_id = found[0]
+
+    title = f"{clean_formula_tags(phase[1])} *{phase[2]}"
+    #title = f"{formula_to_chars(phase[1])} *{phase[2]}" # FIXME entities are not rendered properly
+    descr = f"{PHASE_DESCR}{phase[0]}"
+
+    tag_id = get_or_create_tag(db.connection, db.cursor, USERID, TYPEID, title, descr)
+
+    db.cursor.execute("""INSERT INTO {} ("collectionId", "dataSourceId") VALUES ({}, {}) ON CONFLICT DO NOTHING;""".format(
+        BFF_TABLES['collections_datasources'], tag_id, datasrc_id
+    ))
+    db.connection.commit()
+
+
+if __name__ == "__main__":
+    pass
